bd09840dbb599db18f56c1a7be67f948c71a140b
Farkhanisme <152283396+Farkhanisme@users.noreply.github.com>
2024-07-29 16:01:28 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,241 @@
+<!DOCTYPE html>
+<html lang="in">
+<head>
+
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sewa Jeep Dieng - Explore Keindahan Alam Dieng</title>
+    <meta name="description" content="Sewa jeep Dieng, harga murah untuk menikmati keindahan Negeri Diatas Awan Dieng dengan berbagai paket mulai dari pribadi, keluarga bahkan rombongan">
+    <meta name="keywords" content="sewa jeep dieng plateau, dieng, dieng plateau, penyewaan jeep, wisata dieng">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+
+</head>
+<body>
+
+    <nav class="navbar navbar-expand-md bg-dark sticky-top border-bottom" data-bs-theme="dark">
+
+        <div class="container">
+            <a class="navbar-brand" href="#">
+            <img src="asset/img/logo-nav.svg" alt="jeep" srcset="" width="32" height="32">
+                sewajeepdieng.id
+            </a>
+
+            <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#navbarOffcanvasLg" aria-controls="navbarOffcanvasLg" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <div class="offcanvas offcanvas-end w-100" tabindex="-1" id="navbarOffcanvasLg" aria-labelledby="navbarOffcanvasLgLabel">
+                <div class="offcanvas-header">
+                    <h5 class="offcanvas-title" id="offcanvasNavbarLabel">sewajeepdieng.id</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+                </div>
+                <div class="offcanvas-body">
+                    <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="#">Home</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Harga</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Paket Wisata</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Artikel</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+
+    </nav>
+
+    <main>
+
+        <div class="position-relative overflow-hidden m-md-3 text-center text-light">
+            <img id="banner-background" src="asset/img/banner.jpg" alt="Sewa Jeep Dieng" class="position-absolute top-0 start-0 w-100 h-100 object-fit-cover">
+            <div class="z-1 position-relative col-md-6 p-lg-5 mx-auto my-5">
+                <h1 class="display-3 fw-bold">Sewa Jeep Dieng</h1>
+                <p class="h4 fw-normal text-light mb-3">Tempat penyewaan Jeep Dieng yang Terpercaya</p>
+                <div class="d-flex gap-5 justify-content-center lead fw-normal">
+                    <a class="link-light link-offset-2 link-offset-3-hover link-underline link-underline-opacity-0 link-underline-opacity-75-hover" href="#">
+                    Pesan & Harga
+                    </a>
+                    <a class="link-light link-offset-2 link-offset-3-hover link-underline link-underline-opacity-0 link-underline-opacity-75-hover" href="#">
+                        Paket Wisata
+                    </a>
+                </div>
+            </div>
+        </div>
+        
+        <div id="main-content">
+            <div class="container">
+                <div class="row row-cols-1 row-cols-sm-2 g-3 my-3 py-5">
+                    <div class="col mb-5">
+                        <div id="banner" class="bg-dark text-white position-relative d-flex align-items-center justify-content-center">
+                            <div id="bg-border-1" class="position-absolute top-0 start-0 bottom-0 end-0"></div>
+                            <div id="bg-border-2" class="position-absolute top-0 start-90 bottom-0 end-0"></div>
+                            <div id="label-zona" class="position-relative z-1">
+                                <p class="display-4 fw-semibold mb-0">Zona Wisata</p>
+                                <h2 class="display-3 fw-bold">Sewa Jeep Dieng</h2>
+                            </div>
+                        </div>
+                        <div class="position-relative">
+                            <img src="asset/img/paket-keluarga-1.jpg" alt="penyewaan jeep untuk paket wisata keluarga" class="w-100 h-auto object-fit-cover">
+                            <div class="position-absolute top-0 start-0 w-100 h-100 bg-dark opacity-50"></div>
+                    </div>
+                    </div>
+                    <div class="col">
+                        <div class="row row-cols-1 row-cols-sm-2 g-3">
+                            <div class="col">
+                                <div class="card mb-3">
+                                    <img src="asset/img/zona-1.jpg" class="object-fit-cover card-img-top" height="200px" alt="Wisata Zona 1">
+                                    <div class="card-header text-center bg-dark text-white">
+                                        <h3 class="card-title h4">Zona 1</h3>
+                                    </div>
+                                    <div class="card-body px-0">
+                                        <ol class="list-group list-group-flush list-group-numbered">
+                                            <li class="list-group-item">Candi Arjuna</li>
+                                            <li class="list-group-item">Kawah Sikidang</li>
+                                            <li class="list-group-item">Dieng Theater</li>
+                                            <li class="list-group-item">Batu pandang Ratapan Angin</li>
+                                        </ol>
+                                    </div>
+                                    <div class="card-footer bg-dark text-white d-flex justify-content-between align-items-center">
+                                        <strong>IDR 600k/Unit</strong>
+                                        <button class="btn btn-primary">Pesan</button>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col">
+                                <div class="card mb-3">
+                                    <img src="asset/img/zona-2.jpg" class="object-fit-cover card-img-top" height="200px" alt="Wisata Zona 2">
+                                    <div class="card-header text-center bg-dark text-white">
+                                        <h3 class="card-title h4">Zona 2</h3>
+                                    </div>
+                                    <div class="card-body px-0">
+                                        <ol class="list-group list-group-flush list-group-numbered">
+                                            <li class="list-group-item">Sidringo (Ranokumbolo Dieng)</li>
+                                            <li class="list-group-item">Kawah Candradimuka</li>
+                                            <li class="list-group-item">Sumur Jalatunda</li>
+                                            <li class="list-group-item">Tol Kayangan Sigemplong</li>
+                                        </ol>
+                                    </div>
+                                    <div class="card-footer bg-dark text-white d-flex justify-content-between align-items-center">
+                                        <strong>IDR 750k/Unit</strong>
+                                        <button class="btn btn-primary">Pesan</button>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col">
+                                <div class="card mb-3">
+                                    <img src="asset/img/zona-3.jpg" class="object-fit-cover card-img-top" height="200px" alt="Wisata Zona 3">
+                                    <div class="card-header text-center bg-dark text-white">
+                                        <h3 class="card-title h4">Zona 3</h3>
+                                    </div>
+                                    <div class="card-body px-0">
+                                        <ol class="list-group list-group-flush list-group-numbered">
+                                            <li class="list-group-item">Air Terjun Sikarim</li>
+                                            <li class="list-group-item">Jalan Swiss Van Java</li>
+                                            <li class="list-group-item">Telaga Menjer</li>
+                                            <li class="list-group-item">Kebun Teh Panama</li>
+                                        </ol>
+                                    </div>
+                                    <div class="card-footer bg-dark text-white d-flex justify-content-between align-items-center">
+                                        <strong>IDR 900k/Unit</strong>
+                                        <button class="btn btn-primary">Pesan</button>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col">
+                                <div class="card border-light text-bg-dark mb-3">
+                                    <div class="card-body pt-2 pb-0 text-center">
+                                        <h3>Zona Lain</h3>
+                                    </div>
+                                    <ul class="list-group">
+                                        <li class="list-group-item d-flex align-items-center p-1 gap-2">
+                                            <img src="asset/img/zona-sunrise.jpg" alt="zona sunrise" class="w-25 object-fit-cover" height="50">
+                                            <p>Zona Sunrise</p>
+                                        </li>
+                                        <li class="list-group-item d-flex align-items-center p-1 gap-2">
+                                            <img src="asset/img/zona-healing.jpg" alt="zona healing" class="w-25 object-fit-cover" height="50">
+                                            <p>Zona Healing</p>
+                                        </li>
+                                        <li class="list-group-item d-flex align-items-center p-1 gap-2">
+                                            <img src="asset/img/zona-happy.jpg" alt="zona happy" class="w-25 object-fit-cover" height="50">
+                                            <p>Zona Happy</p>
+                                        </li>
+                                    </ul>
+
+                                    <div class="card-footer">
+                                        <a class="link-light link-offset-2 link-offset-3-hover link-underline link-underline-opacity-0 link-underline-opacity-75-hover" href="paket.html">Lihat lainnya...</a>
+                                    </div>
+
+                                </div>
+                            </div>
+                                
+                        </div>
+                    </div>
+                        
+                </div>
+            </div>
+            
+
+        </div>
+
+    </main>
+
+    <footer class="p-5 mt-5 bg-body-tertiary">
+        <div class="row">
+            <div class="col-12 col-md d-flex flex-column align-items-center">
+                <img src="asset/img/logo-foot.svg" alt="jeep" srcset="" width="32" height="32">
+                <small class="d-block mb-3 text-body-secondary">&copy; sewajeepdieng.id 2024</small>
+            </div>
+            <div class="col-6 col-md">
+                <h5>Media Sosial</h5>
+                <ul class="list-unstyled text-small">
+                    <li><a class="link-secondary text-decoration-none" href="#">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-facebook" viewBox="0 0 16 16"><title>Facebook</title>
+                        <path d="M16 8.049c0-4.446-3.582-8.05-8-8.05C3.58 0-.002 3.603-.002 8.05c0 4.017 2.926 7.347 6.75 7.951v-5.625h-2.03V8.05H6.75V6.275c0-2.017 1.195-3.131 3.022-3.131.876 0 1.791.157 1.791.157v1.98h-1.009c-.993 0-1.303.621-1.303 1.258v1.51h2.218l-.354 2.326H9.25V16c3.824-.604 6.75-3.934 6.75-7.951"/>
+                    </svg>
+                        Facebook
+                    </a></li>
+                    <li><a class="link-secondary text-decoration-none" href="#">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-instagram" viewBox="0 0 16 16"><title>Instagram</title>
+                        <path d="M8 0C5.829 0 5.556.01 4.703.048 3.85.088 3.269.222 2.76.42a3.9 3.9 0 0 0-1.417.923A3.9 3.9 0 0 0 .42 2.76C.222 3.268.087 3.85.048 4.7.01 5.555 0 5.827 0 8.001c0 2.172.01 2.444.048 3.297.04.852.174 1.433.372 1.942.205.526.478.972.923 1.417.444.445.89.719 1.416.923.51.198 1.09.333 1.942.372C5.555 15.99 5.827 16 8 16s2.444-.01 3.298-.048c.851-.04 1.434-.174 1.943-.372a3.9 3.9 0 0 0 1.416-.923c.445-.445.718-.891.923-1.417.197-.509.332-1.09.372-1.942C15.99 10.445 16 10.173 16 8s-.01-2.445-.048-3.299c-.04-.851-.175-1.433-.372-1.941a3.9 3.9 0 0 0-.923-1.417A3.9 3.9 0 0 0 13.24.42c-.51-.198-1.092-.333-1.943-.372C10.443.01 10.172 0 7.998 0zm-.717 1.442h.718c2.136 0 2.389.007 3.232.046.78.035 1.204.166 1.486.275.373.145.64.319.92.599s.453.546.598.92c.11.281.24.705.275 1.485.039.843.047 1.096.047 3.231s-.008 2.389-.047 3.232c-.035.78-.166 1.203-.275 1.485a2.5 2.5 0 0 1-.599.919c-.28.28-.546.453-.92.598-.28.11-.704.24-1.485.276-.843.038-1.096.047-3.232.047s-2.39-.009-3.233-.047c-.78-.036-1.203-.166-1.485-.276a2.5 2.5 0 0 1-.92-.598 2.5 2.5 0 0 1-.6-.92c-.109-.281-.24-.705-.275-1.485-.038-.843-.046-1.096-.046-3.233s.008-2.388.046-3.231c.036-.78.166-1.204.276-1.486.145-.373.319-.64.599-.92s.546-.453.92-.598c.282-.11.705-.24 1.485-.276.738-.034 1.024-.044 2.515-.045zm4.988 1.328a.96.96 0 1 0 0 1.92.96.96 0 0 0 0-1.92m-4.27 1.122a4.109 4.109 0 1 0 0 8.217 4.109 4.109 0 0 0 0-8.217m0 1.441a2.667 2.667 0 1 1 0 5.334 2.667 2.667 0 0 1 0-5.334"/>
+                    </svg>
+                    Instagram
+                    </a></li>
+                    <li><a class="link-secondary text-decoration-none" href="#">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-tiktok" viewBox="0 0 16 16"><title>Tiktok</title>
+                        <path d="M9 0h1.98c.144.715.54 1.617 1.235 2.512C12.895 3.389 13.797 4 15 4v2c-1.753 0-3.07-.814-4-1.829V11a5 5 0 1 1-5-5v2a3 3 0 1 0 3 3z"/>
+                    </svg>
+                    Tiktok
+                    </a></li>
+                </ul>
+            </div>
+                <div class="col-6 col-md">
+                <h5>Kontak</h5>
+                <ul class="list-unstyled text-small">
+                    <li><a class="link-secondary text-decoration-none" href="#">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-whatsapp" viewBox="0 0 16 16"><title>WhatsApp</title>
+                        <path d="M13.601 2.326A7.85 7.85 0 0 0 7.994 0C3.627 0 .068 3.558.064 7.926c0 1.399.366 2.76 1.057 3.965L0 16l4.204-1.102a7.9 7.9 0 0 0 3.79.965h.004c4.368 0 7.926-3.558 7.93-7.93A7.9 7.9 0 0 0 13.6 2.326zM7.994 14.521a6.6 6.6 0 0 1-3.356-.92l-.24-.144-2.494.654.666-2.433-.156-.251a6.56 6.56 0 0 1-1.007-3.505c0-3.626 2.957-6.584 6.591-6.584a6.56 6.56 0 0 1 4.66 1.931 6.56 6.56 0 0 1 1.928 4.66c-.004 3.639-2.961 6.592-6.592 6.592m3.615-4.934c-.197-.099-1.17-.578-1.353-.646-.182-.065-.315-.099-.445.099-.133.197-.513.646-.627.775-.114.133-.232.148-.43.05-.197-.1-.836-.308-1.592-.985-.59-.525-.985-1.175-1.103-1.372-.114-.198-.011-.304.088-.403.087-.088.197-.232.296-.346.1-.114.133-.198.198-.33.065-.134.034-.248-.015-.347-.05-.099-.445-1.076-.612-1.47-.16-.389-.323-.335-.445-.34-.114-.007-.247-.007-.38-.007a.73.73 0 0 0-.529.247c-.182.198-.691.677-.691 1.654s.71 1.916.81 2.049c.098.133 1.394 2.132 3.383 2.992.47.205.84.326 1.129.418.475.152.904.129 1.246.08.38-.058 1.171-.48 1.338-.943.164-.464.164-.86.114-.943-.049-.084-.182-.133-.38-.232"/>
+                    </svg>
+                    WhatsApp
+                    </a></li>
+                    <li><a class="link-secondary text-decoration-none" href="#">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-telegram" viewBox="0 0 16 16"><title>Telegram</title>
+                        <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0M8.287 5.906q-1.168.486-4.666 2.01-.567.225-.595.442c-.03.243.275.339.69.47l.175.055c.408.133.958.288 1.243.294q.39.01.868-.32 3.269-2.206 3.374-2.23c.05-.012.12-.026.166.016s.042.12.037.141c-.03.129-1.227 1.241-1.846 1.817-.193.18-.33.307-.358.336a8 8 0 0 1-.188.186c-.38.366-.664.64.015 1.088.327.216.589.393.85.571.284.194.568.387.936.629q.14.092.27.187c.331.236.63.448.997.414.214-.02.435-.22.547-.82.265-1.417.786-4.486.906-5.751a1.4 1.4 0 0 0-.013-.315.34.34 0 0 0-.114-.217.53.53 0 0 0-.31-.093c-.3.005-.763.166-2.984 1.09"/>
+                    </svg>    
+                    Telegram
+                    </a></li>
+                </ul>
+            </div>
+        </div>
+    </footer>
+
+</body>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+</html>
